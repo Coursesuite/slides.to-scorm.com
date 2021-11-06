998695349c7620dd72b026335d6bacbd10380876
TIm St.Clair <tim.stclair@gmail.com>
2021-11-06 17:20:38 +1100

    added metadata

--- public/index.php
+++ public/index.php
@@ -459,9 +459,11 @@ if (is_array($SlideArray)) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <title>Slides to Scorm</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Slides to Scorm</title>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <meta name="description" content="A free tool to let you convert PPTX, KEYNOTE, GOOGLE SLIDES to an automatic web-based slideshow, with audio or video per slide. SCORM compatible.">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
     <link rel="stylesheet" href="app.css" type="text/css">
 </head>
