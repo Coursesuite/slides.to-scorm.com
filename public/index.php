44d1687914875bc1f3f9457f55de2d46534f1179
TIm St.Clair <tim.stclair@gmail.com>
2021-11-07 16:16:11 +1100

    updated footer

--- public/index.php
+++ public/index.php
@@ -581,7 +581,9 @@ if ($downloadable) { ?>
 
 
     <footer>
-    <?php echo "ID: {$id}"; ?> <a href="?reset=1">Reset</a> <a href="https://www.courseassembler.com" style="color:inherit">Try CourseAssembler</a>
+    <p><?php echo "ID: {$id}"; ?> <a href="?reset=1">Reset</a></p>
+    <p><a href="https://pdf.to-scorm.com/">PDF 2 Scorm</a> | <a href="https://youtube.to-scorm.com/">YouTube 2 Scorm</a> | <a href="https://vimeo.to-scorm.com/">Vimeo 2 Scorm</a> | <a href="https://soundcloud.to-scorm.com/">SoundCloud 2 Scorm</a> | <a href="https://presentation.to-scorm.com/">Audio+Presentation 2 Scorm</a> | <a href="https://video.to-scorm.com/">Video 2 Scorm</a> | <a href="https://slides.to-scorm.com/">Slides 2 Scorm</a></p>
+            <p>Need something more comprehensive? Try <a href="https://www.courseassembler.com/">Course Assembler</a>.</p>
     </footer>
 
     <script src="https://unpkg.com/mic-recorder-to-mp3"></script>
